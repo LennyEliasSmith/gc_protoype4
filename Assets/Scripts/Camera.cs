068f5e2405859579b705d64162799b4118e8e9ff
Eero <Salmi>
2020-06-28 16:20:25 +0300

    Camera script to look at birb

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject birb;
+    public Camera mainCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mainCam.transform.LookAt(birb.transform);
+        
+    }
+}
